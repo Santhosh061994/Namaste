3983dc24ef3fd2231b33ac45deaeb1aa9c814b76
Santhosh061994 <sunnysanthosh04@gmail.com>
2024-07-14 09:18:28 +0530

    commiting my first file

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", { id: "heading", xyz: "abc" }, "Hello World from React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement("div", { id: "parent" },
     [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am an h1 tag"), React.createElement("h2", {}, ("I am an h2 tag"))]),
+        [React.createElement("h1", {}, "Santhosh 💕 Sahanya"), React.createElement("h2", {}, ("Namaste react❤️"))]),
     React.createElement("div", { id: "child2" },
         [React.createElement("h3", {}, "I am an h3 tag"), React.createElement("h14", {}, ("I am an h4 tag"))])]);
 
